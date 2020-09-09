2997ac904301926ee5eb88297296c08a994c803f
Marcel Unkauf <unka@itu.dk>
2020-09-09 11:24:09 +0200

    server is responding

--- QuestionableDatagramSocket.java
+++ QuestionableDatagramSocket.java
@@ -1,27 +1,42 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
-import sun.tools.jstat.RawOutputFormatter;
-
+import java.util.*;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
+import java.net.*;
 
 public class QuestionableDatagramSocket extends DatagramSocket {
 
-    Random random = new Random();
+    private Random random = new Random();
 
     enum Response {
         DISCARD, DUPLICATE, REORDER, SEND
     }
 
-    QuestionableDatagramSocket() throws SocketException {
+    QuestionableDatagramSocket() throws SocketException{
+        super();
+    }
+
+    QuestionableDatagramSocket(int i) throws SocketException {
+        super(i);
+
+    }
+
+    @Override
+    public synchronized void receive(DatagramPacket p) throws IOException {
+        super.receive(p);
+        String s = new String(p.getData());
+        
+        // Logic
+        System.out.println("QuestionableDatagramSocket: " + s);
+
+    }
+
+    @Override
+    public void send(DatagramPacket p) throws IOException {
+        // TODO Auto-generated method stub
+        super.send(p);
 
         Response[] responses = Response.values();
         int pickResponse = random.nextInt(4);
-
+        
         switch (responses[pickResponse]) {
             case DISCARD:
                 System.out.println("DISCARD");
@@ -38,22 +53,6 @@ public class QuestionableDatagramSocket extends DatagramSocket {
             default:
                 break;
         }
-
-    }
-
-    @Override
-    public synchronized void receive(DatagramPacket p) throws IOException {
-        super.receive(p);
-        String s = new String(p.getData());
-
-        // Logic
-
-    }
-
-    @Override
-    public void send(DatagramPacket p) throws IOException {
-        // TODO Auto-generated method stub
-        super.send(p);
     }
 
     public static void main(String[] args) {
