bde6d3f78b64bb3f653f0080a00bf96e57113a34
Emil Baehr <emil.baehr@gmail.com>
2020-09-09 12:56:20 +0200

    remove unneeded fields and methods

--- QuestionableDatagramSocket.java
+++ QuestionableDatagramSocket.java
@@ -5,13 +5,13 @@ import java.net.*;
 public class QuestionableDatagramSocket extends DatagramSocket {
 
     private Random random = new Random();
-    //private String[] words;
+    // private String[] words;
 
     enum Response {
         DISCARD, DUPLICATE, REORDER, SEND
     }
 
-    QuestionableDatagramSocket() throws SocketException{
+    QuestionableDatagramSocket() throws SocketException {
         super();
     }
 
@@ -19,28 +19,19 @@ public class QuestionableDatagramSocket extends DatagramSocket {
         super(i);
     }
 
-    @Override //not needed I think
-    public synchronized void receive(DatagramPacket p) throws IOException {
-        super.receive(p);
-        String s = new String(p.getData());
-
-        // Logic
-        String[] words = s.split(" ");    
-    }
-
     @Override
     public void send(DatagramPacket p) throws IOException {
         // TODO Auto-generated method stub
         Response[] responses = Response.values();
         int pickResponse = random.nextInt(4);
-        
+
         String s = new String(p.getData());
         String[] words = s.split(" ");
         String respond = new String();
 
         switch (responses[pickResponse]) {
             case DISCARD:
-                //;
+                System.out.println("DUPLICATE");
                 break;
             case DUPLICATE:
                 System.out.println("DUPLICATE");
@@ -58,13 +49,6 @@ public class QuestionableDatagramSocket extends DatagramSocket {
         byte[] msg = s.getBytes();
         p.setData(msg);
         super.send(p);
-    }
-
-    public static void main(String[] args) {
-        try {
-            QuestionableDatagramSocket socket = new QuestionableDatagramSocket();
-        } catch (SocketException e) { }
-
     }
 
 }
